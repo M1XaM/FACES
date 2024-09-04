c33521211c0bd4606721a75aad8b56a7bcffba67
George2973 <gelu6563@gmail.com>
2024-09-04 12:50:07 +0300

    work on header

--- Dropdown.js
+++ Dropdown.js
@@ -1,23 +1,39 @@
+function toggleDropdown(dropdownId, iconSelector) {
+    const dropdown = document.getElementById(dropdownId);
+    const icon = document.querySelector(iconSelector);
+
+    dropdown.classList.toggle("show");
+
+    if (dropdown.classList.contains("show")) {
+        icon.classList.add("rotate");
+    } else {
+        icon.classList.remove("rotate");
+    }
+}
+
 function MyFunction() {
-    document.getElementById("MyDropdown").classList.toggle("show");
+    toggleDropdown("MyDropdown", ".dropdown .fa-arrow-up");
 }
 
 function MyFunctiontwo() {
-    document.getElementById("MyDropdown2").classList.toggle("show");
+    toggleDropdown("MyDropdown2", ".dropdown2 .fa-arrow-up");
+
 }
 
 window.onclick = function(event) {
-    if (!event.target.matches('.dropbtn')) {
+    if (!event.target.matches('.dropbtn') && !event.target.matches('.fa-arrow-up')) {
         var dropdowns = document.getElementsByClassName("dropdown-content");
-        var dropdowns2 = document.getElementsByClassName("dropdown-content2");
-        
-        for (var i = 0; i < dropdowns.length; i++) {    
+        var icons = document.querySelectorAll(".dropdown .fa-arrow-up");
+
+        for (var i = 0; i < dropdowns.length; i++) {
             var openDropdown = dropdowns[i];
             if (openDropdown.classList.contains('show')) {
                 openDropdown.classList.remove('show');
+                icons[i].classList.remove("rotate");
             }
         }
-        
+
+        var dropdowns2 = document.getElementsByClassName("dropdown-content2");
         for (var j = 0; j < dropdowns2.length; j++) {
             var openDropdown2 = dropdowns2[j];
             if (openDropdown2.classList.contains('show')) {
@@ -26,11 +42,3 @@ window.onclick = function(event) {
         }
     }
 }
-
-document.getElementById("nav__links").addEventListener("click", function(e) {
-    const tgt = e.target;
-    if (tgt.classList.contains('fa-arrow-up')) { 
-      tgt.classList.toggle('open');
-    }
-  });
-
