d21c0413a2a1da8622904e6f63c7c512685806da
George2973 <gelu6563@gmail.com>
2024-09-04 13:04:40 +0300

    solved the Dropdown button, need to align

--- Dropdown.js
+++ Dropdown.js
@@ -6,7 +6,9 @@ function toggleDropdown(dropdownId, iconSelector) {
 
     if (dropdown.classList.contains("show")) {
         icon.classList.add("rotate");
+        icon.classList.remove("rotate-back");
     } else {
+        icon.classList.add("rotate-back");
         icon.classList.remove("rotate");
     }
 }
@@ -17,27 +19,30 @@ function MyFunction() {
 
 function MyFunctiontwo() {
     toggleDropdown("MyDropdown2", ".dropdown2 .fa-arrow-up");
-
 }
 
 window.onclick = function(event) {
     if (!event.target.matches('.dropbtn') && !event.target.matches('.fa-arrow-up')) {
         var dropdowns = document.getElementsByClassName("dropdown-content");
+        var dropdowns2 = document.getElementsByClassName("dropdown-content2");
         var icons = document.querySelectorAll(".dropdown .fa-arrow-up");
+        var icons2 = document.querySelectorAll(".dropdown2 .fa-arrow-up");
 
         for (var i = 0; i < dropdowns.length; i++) {
             var openDropdown = dropdowns[i];
             if (openDropdown.classList.contains('show')) {
                 openDropdown.classList.remove('show');
+                icons[i].classList.add("rotate-back");
                 icons[i].classList.remove("rotate");
             }
         }
 
-        var dropdowns2 = document.getElementsByClassName("dropdown-content2");
         for (var j = 0; j < dropdowns2.length; j++) {
             var openDropdown2 = dropdowns2[j];
             if (openDropdown2.classList.contains('show')) {
                 openDropdown2.classList.remove('show');
+                icons2[j].classList.add("rotate-back");
+                icons2[j].classList.remove("rotate");
             }
         }
     }
