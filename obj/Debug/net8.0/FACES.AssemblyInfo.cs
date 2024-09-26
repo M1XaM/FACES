ae39174eb920190dc138aa08f8c8f48eead11719
Isacescu Maxim <isacescu.maxim12@gmail.com>
2024-09-26 09:38:13 +0300

    Adjusting .gitignore

--- obj/Debug/net8.0/FACES.AssemblyInfo.cs
+++ obj/Debug/net8.0/FACES.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FACES")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7640bf817b8627e7564c9bc7a34310008a8f8972")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+301320dcb23747e8826df527087aeccc7780bbb7")]
 [assembly: System.Reflection.AssemblyProductAttribute("FACES")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FACES")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
